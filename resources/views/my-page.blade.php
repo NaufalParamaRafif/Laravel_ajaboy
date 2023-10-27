d4bc05f72358bc40892837680dbd3beb24923536
NaufalParamaRafif <profesoraieuo@gmail.com>
2023-10-27 07:53:48 +0700

    first commit

---
+++ resources/views/my-page.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mamang Jualan</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="card" style="width: 18rem;">
+      <div class="card-header">
+        Buku
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Author: Raditya Dika</li>
+        <li class="list-group-item">Buku: Marmut Merah Jambu</li>
+        <li class="list-group-item">Genre: Fiksi</li>
+      </ul>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Author: How to Read a Book</li>
+        <li class="list-group-item">Buku: Mortimer Adler & Charles van Doren</li>
+        <li class="list-group-item">Genre: Non Fiksi</li>
+      </ul>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Author: Naufal Parama Rafif</li>
+        <li class="list-group-item">Buku: Membuat AI dalam satu menit</li>
+        <li class="list-group-item">Genre: Fiksi Ilmiah</li>
+      </ul>
+    </div>
+  </body>
+</html>
